dfa37cb9a313d9b43a0ca45836b24214b0333a22
Andrew Wooldridge <awooldridge@ebay.com>
2019-01-04 16:19:48 -0800

    working on the site

--- src/.eleventy.js
+++ src/.eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("img");
+    eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addPassthroughCopy("uikit");
     return {
         templateFormats: [
