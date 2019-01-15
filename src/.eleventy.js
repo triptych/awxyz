6a42ea194f4630c3f53f58d1d66744b3e33c3d28
Andrew Wooldridge <awooldridge@ebay.com>
2019-01-15 15:59:53 -0800

    getting posts to work

--- src/.eleventy.js
+++ src/.eleventy.js
@@ -1,7 +1,46 @@
+const { DateTime } = require("luxon");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
 module.exports = function (eleventyConfig) {
+
+    // plugins
+    eleventyConfig.addPlugin(pluginRss);
+    eleventyConfig.addPlugin(pluginSyntaxHighlight);
+    eleventyConfig.setDataDeepMerge(true);
+
+    // aliases
+    eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
+
+    // filters
+    eleventyConfig.addFilter("readableDate", dateObj => {
+        return DateTime.fromJSDate(dateObj, {
+            zone: 'utc'
+        }).toFormat("dd LLL yyyy");
+    });
+
+    // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+    eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toFormat('yyyy-LL-dd');
+    });
+
+    
+    // Get the first `n` elements of a collection.
+    eleventyConfig.addFilter("head", (array, n) => {
+        if (n < 0) {
+            return array.slice(n);
+        }
+
+        return array.slice(0, n);
+    });
+
+    // collections
+    eleventyConfig.addCollection("tagList", require("./_11ty/getTagList"));
+
     eleventyConfig.addPassthroughCopy("img");
     eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addPassthroughCopy("uikit");
+
     return {
         templateFormats: [
             "md",
---
+++ src/posts/firstpost.md
@@ -0,0 +1,10 @@
+---
+title: This is my first post
+description: This is my first post to this new static site I'm working on
+date: 2019-01-15
+tags:
+ - another-tag
+layout: layouts/post.njk
+---
+This is a test of the new statically generated site I'm working on.
+
